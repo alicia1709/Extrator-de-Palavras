25565c1197cd18ae35c49cc30625b1ad84208c93
alicia1709 <r.alicia@escola.pr.gov.br>
2025-08-17 12:59:59 -0300

    fui clicado

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaomostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaomostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave (){
+    alert ("Fui clicado!");
+}
